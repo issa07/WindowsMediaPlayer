7e97b7ee15c5ad43d0bd70d04fc460247aa3d7ff
issa07 <issa9696@hotmail.fr>
2021-10-02 22:00:45 +0200

    fonctionnalité ajouté : play,stop,pause,resume,reverse,forward,previous,next,search by name,

--- WindowsMediaPlayer/Form1.Designer.cs
+++ WindowsMediaPlayer/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace WindowsMediaPlayer
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,19 +42,32 @@ namespace WindowsMediaPlayer
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button8_track_list = new System.Windows.Forms.Button();
+            this.LBLCOUNT = new System.Windows.Forms.Label();
+            this.import = new System.Windows.Forms.OpenFileDialog();
+            this.pic_art = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.track_list = new System.Windows.Forms.ListBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.radioBtn_nom = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_art)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // axWindowsMediaPlayer1
             // 
-            this.axWindowsMediaPlayer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axWindowsMediaPlayer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.axWindowsMediaPlayer1.Enabled = true;
             this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(-1, 64);
             this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
-            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(796, 589);
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(421, 423);
             this.axWindowsMediaPlayer1.TabIndex = 0;
             // 
             // label1
@@ -71,15 +85,13 @@ namespace WindowsMediaPlayer
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(120, 19);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(156, 26);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Size = new System.Drawing.Size(139, 26);
+            this.textBox1.TabIndex = 10;
             // 
             // button1
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(283, 12);
+            this.button1.Location = new System.Drawing.Point(265, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(73, 40);
             this.button1.TabIndex = 3;
@@ -89,92 +101,211 @@ namespace WindowsMediaPlayer
             // 
             // button2
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.button2.BackColor = System.Drawing.Color.Lime;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(362, 12);
+            this.button2.Location = new System.Drawing.Point(344, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(76, 40);
             this.button2.TabIndex = 4;
             this.button2.Text = "Play";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.button3.BackColor = System.Drawing.Color.IndianRed;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(444, 12);
+            this.button3.Location = new System.Drawing.Point(426, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(76, 40);
             this.button3.TabIndex = 5;
             this.button3.Text = "Stop";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.button4.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(526, 11);
+            this.button4.Location = new System.Drawing.Point(508, 12);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(76, 43);
+            this.button4.Size = new System.Drawing.Size(76, 41);
             this.button4.TabIndex = 6;
             this.button4.Text = "Pause";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(608, 12);
+            this.button5.Location = new System.Drawing.Point(590, 13);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 40);
+            this.button5.Size = new System.Drawing.Size(75, 39);
             this.button5.TabIndex = 7;
             this.button5.Text = "Resume";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(689, 11);
+            this.button6.Location = new System.Drawing.Point(671, 12);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(42, 43);
-            this.button6.TabIndex = 8;
+            this.button6.Size = new System.Drawing.Size(35, 40);
+            this.button6.TabIndex = 1;
             this.button6.Text = "<<";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(737, 12);
+            this.button7.Location = new System.Drawing.Point(712, 12);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(40, 40);
-            this.button7.TabIndex = 9;
+            this.button7.Size = new System.Drawing.Size(33, 41);
+            this.button7.TabIndex = 0;
             this.button7.Text = ">>";
-            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // button8_track_list
+            // 
+            this.button8_track_list.BackColor = System.Drawing.Color.DarkGray;
+            this.button8_track_list.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8_track_list.Location = new System.Drawing.Point(751, 12);
+            this.button8_track_list.Name = "button8_track_list";
+            this.button8_track_list.Size = new System.Drawing.Size(116, 41);
+            this.button8_track_list.TabIndex = 11;
+            this.button8_track_list.Text = "Parcourir";
+            this.button8_track_list.UseVisualStyleBackColor = false;
+            this.button8_track_list.Click += new System.EventHandler(this.btn_track_list_open);
+            // 
+            // LBLCOUNT
+            // 
+            this.LBLCOUNT.Location = new System.Drawing.Point(0, 0);
+            this.LBLCOUNT.Name = "LBLCOUNT";
+            this.LBLCOUNT.Size = new System.Drawing.Size(100, 23);
+            this.LBLCOUNT.TabIndex = 14;
+            // 
+            // pic_art
+            // 
+            this.pic_art.Image = global::WindowsMediaPlayer.Properties.Resources.disc;
+            this.pic_art.Location = new System.Drawing.Point(420, 64);
+            this.pic_art.Name = "pic_art";
+            this.pic_art.Size = new System.Drawing.Size(281, 423);
+            this.pic_art.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_art.TabIndex = 13;
+            this.pic_art.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(873, 491);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Playlist";
+            // 
+            // track_list
+            // 
+            this.track_list.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.track_list.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.track_list.FormattingEnabled = true;
+            this.track_list.ItemHeight = 20;
+            this.track_list.Location = new System.Drawing.Point(701, 64);
+            this.track_list.Name = "track_list";
+            this.track_list.Size = new System.Drawing.Size(229, 424);
+            this.track_list.TabIndex = 16;
+            this.track_list.SelectedIndexChanged += new System.EventHandler(this.track_list_SelectedIndexChanged);
+            // 
+            // button8
+            // 
+            this.button8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(13, 494);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(87, 43);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "Précedent";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.btn_previous_list);
+            // 
+            // button9
+            // 
+            this.button9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(120, 494);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(87, 43);
+            this.button9.TabIndex = 18;
+            this.button9.Text = "Suivant";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.btn_next_list);
+            // 
+            // radioBtn_nom
+            // 
+            this.radioBtn_nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtn_nom.Location = new System.Drawing.Point(701, 503);
+            this.radioBtn_nom.Name = "radioBtn_nom";
+            this.radioBtn_nom.Size = new System.Drawing.Size(104, 24);
+            this.radioBtn_nom.TabIndex = 23;
+            this.radioBtn_nom.Text = "Par Artiste";
+            this.radioBtn_nom.CheckedChanged += new System.EventHandler(this.radioBtn_par_nom);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(701, 571);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(205, 52);
+            this.radioButton2.TabIndex = 22;
+            this.radioButton2.Text = "Par l\'ordre alphabétique";
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(738, 533);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 21;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(795, 654);
+            this.ClientSize = new System.Drawing.Size(942, 635);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioBtn_nom);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.track_list);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pic_art);
+            this.Controls.Add(this.LBLCOUNT);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button8_track_list);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_art)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -193,6 +324,18 @@ namespace WindowsMediaPlayer
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button8_track_list;
+        private System.Windows.Forms.Label LBLCOUNT;
+        private System.Windows.Forms.OpenFileDialog import;
+        private System.Windows.Forms.PictureBox pic_art;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox track_list;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RadioButton radioBtn_nom;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.BindingSource bindingSource1;
     }
 }
 
--- WindowsMediaPlayer/Form1.cs
+++ WindowsMediaPlayer/Form1.cs
@@ -17,11 +17,7 @@ namespace WindowsMediaPlayer
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
+        // Pour ouvrir le fichier 
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog opf = new OpenFileDialog();
@@ -30,5 +26,137 @@ namespace WindowsMediaPlayer
                 textBox1.Text = opf.FileName;
             }
         }
+
+        // Pour lancer la musique ou la vidéo
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = textBox1.Text;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+
+            Button btnSender = (Button)sender;
+        }
+
+        // Pour arrêter
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        // Pour mettre pause 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        // Reprendre
+        private void button5_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        // Reverse
+        private void button6_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.fastReverse();
+        }
+
+        // Forward
+        private void button7_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.fastForward();
+        }
+
+        String[] paths, files;
+
+        // Pour créer une playlist (btn_track_list)
+        private void btn_track_list_open(object sender, EventArgs e)
+        {
+            // Create ListBox.
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+
+                for (int x = 0; x < files.Length; x++)
+                {
+                    track_list.Items.Add(files[x]);
+                    
+                }
+            }
+
+        }
+
+        // Pour selection une musique parmi la liste (Playlist)
+        private void track_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            axWindowsMediaPlayer1.URL = paths[track_list.SelectedIndex];
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+
+
+            //bindingSource1.Filter = "'%" + textBox2.Text + "%'";           
+        }
+
+        // Bouton pour à la musique précendente
+        private void btn_previous_list(object sender, EventArgs e)
+        {
+            if (track_list.SelectedIndex > 0)
+            {
+                track_list.SelectedIndex = track_list.SelectedIndex - 1;
+            }
+        }
+
+        // Bouton pour passer à la musique suivante
+        private void btn_next_list(object sender, EventArgs e)
+        {
+            if (track_list.SelectedIndex < track_list.Items.Count - 1)
+            {
+                track_list.SelectedIndex = track_list.SelectedIndex + 1;
+            }
+        }
+
+        // Bouton radion "Par artistes"
+        private void radioBtn_par_nom(object sender, EventArgs e)
+        {
+            textBox2.Enabled = true;
+           /* //track_list.Items.Clear();
+            for (int i = track_list.Items.Count - 1; i >= 0; i--)
+            {
+                if (track_list.Items[i].ToString().ToLower().Contains(textBox2.Text.ToLower()))
+                {
+                    track_list.SetSelected(i, true);
+
+                }
+                
+            }*/
+        }
+
+        // Récupérer la valeur saisi de textBox2 puis filtrer
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // BindingSource ofd = new BindingSource();
+            // bindingSource1.Filter = "'%" + textBox2.Text + "%'";
+
+            for (int i = track_list.Items.Count - 1; i >= 0; i--)
+            {
+                if (track_list.Items[i].ToString().ToLower().Contains(textBox2.Text.ToLower()))
+                {
+                    track_list.SetSelected(i, true);
+                }
+            }
+
+        }
+
+        // Filrer par l'ordre alphabétique
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
     }
 }
+
